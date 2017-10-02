c565a062bf002fca7b38d35b877e28947f8ada7c
Bryan <bryanporras2033@gmail.com>
2017-10-02 18:51:39 -0400

    finished home.html and working on survey.html

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ var bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
+PORT = process.env.PORT || 8080
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 require("./app/routing/htmlRoutes.js")(app)
 require("./app/routing/apiRoutes.js")(app)
 
-app.listen(8080, function(){
-    console.log("Currently listening on http://localhost:" + 8080);
+app.listen(PORT,function(){
+    console.log("Currently listening on http://localhost:" + PORT);
 });
